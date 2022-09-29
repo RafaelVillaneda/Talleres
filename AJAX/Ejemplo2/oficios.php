b64d98b55137616e27af4b99263720c4ac1f762e
Lucy <71149886+RafaelVillaneda@users.noreply.github.com>
2022-09-29 18:32:40 -0500

    Se agrego el ejemplo 2 de AJAX

--- AJAX/Ejemplo2/oficios.php
+++ AJAX/Ejemplo2/oficios.php
@@ -1,20 +1,24 @@
 <?php 
-    $oficios=array("Carpintero","Pintor","Youtuber");
-    $oficios=$_GET["nombre_oficio"];
+    $oficios = array("Carpintero","Cerrajero","Mecanico","Electricista","Conductor",
+    "Albañil","Fontanero","Pintor","Tapicero","Camarero","Carnicero",
+    "Pastor","Agricultor","Herrero","Sastre");
 
-    if(strlen($oficios)>0){
-        $resultados="";
-        for($i=0;$i<count($oficios);$i++){
-            if(strtolower($oficios)==strtolower(substr($oficios[$i],0,strlen($oficios)))){
-            if($resultados==""){
-                $resultados=$oficios[$i];
-            }else{
-                $resultados=$resultados." ,".$oficios[$i];
-            }//ELSE
-        }//FOR
+    $oficio = $_GET["nombre_oficio"];
+
+    if(strlen($oficio) > 0){
+        $resultados = "";
+
+        for($i = 0; $i < count($oficios); $i++){
+            if(strtolower($oficio) == strtolower(substr($oficios[$i], 0, strlen($oficio)))){
+                if($resultados == ""){
+                    $resultados = $oficios[$i];
+                } else{
+                    $resultados = $resultados . ", " . $oficios[$i];
+                }
+            }
+        }
     }
-    }//IF
 
-    echo($resultados=="") ?'No se encontraron coincidencias' :$resultados;
+    echo ($resultados == "") ? 'No se encontraron coincidencias' : $resultados;
 
 ?>
